3051bbf05f26b7685189d93ec91cf4bad7dd347c
Johnny <wonjohnny@outlook.com>
2025-11-08 22:33:42 -0800

    update to api event to include a configurable path

--- Engine/EngineCore/include/EngineCore/Scripting/api_event.h
+++ Engine/EngineCore/include/EngineCore/Scripting/api_event.h
@@ -21,14 +21,14 @@ class ApiEvent_0 : public ApiEventBase
 {
 public:
     inline size_t GetParamCount() const override { return 0; }
-    virtual void Run(const Runtime::ServiceTable*, const void*, Runtime::EventWriter*) const = 0;
+    virtual void Run(const Runtime::ServiceTable*, const void*, Runtime::EventWriter*, int) const = 0;
 };
 
 class ApiEventBase_1 : public ApiEventBase
 {
 public:
     inline size_t GetParamCount() const override { return 1; }
-    virtual void Run(const Runtime::ServiceTable* services, const void* moduleState, ApiData p1, Runtime::EventWriter* writer) const = 0;
+    virtual void Run(const Runtime::ServiceTable*, const void*, ApiData, Runtime::EventWriter*, int) const = 0;
     virtual ApiDataDefinition GetP1Type() const = 0;
 };
 
@@ -36,15 +36,15 @@ template <typename TP1>
 class ApiEvent_1 : public ApiEventBase_1
 {
 protected:
-    virtual void RunCore(const Runtime::ServiceTable* services, const void* moduleState, const TP1* p1, Runtime::EventWriter* writer) const = 0;
+    virtual void RunCore(const Runtime::ServiceTable*, const void*, const TP1*, Runtime::EventWriter*, int) const = 0;
 
 public:
-    void Run(const Runtime::ServiceTable* services, const void* moduleState, ApiData p1, Runtime::EventWriter* writer) const override
+    void Run(const Runtime::ServiceTable* services, const void* moduleState, ApiData p1, Runtime::EventWriter* writer, int path) const override
     {
         if (!IsTypeCompiliant<TP1>(services, moduleState, &p1))
             return;
 
-        RunCore(services, moduleState, FromApiData<TP1>(&p1), writer);
+        RunCore(services, moduleState, FromApiData<TP1>(&p1), writer, path);
     }
 
     ApiDataDefinition GetP1Type() const override 
--- Engine/EngineCore/include/EngineCore/Scripting/event_declaration.h
+++ Engine/EngineCore/include/EngineCore/Scripting/event_declaration.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#include <EngineCore/Scripting/api_event.h>
+
 #define DECLARE_SE_EVENT_0(name, returnType, runCore)\
 class name : public Engine::Core::Scripting::ApiEvent_0\
 {\
 public:\
-    void Run(const Runtime::ServiceTable* services, const void* moduleState, Runtime::EventWriter* writer) const override\
+    void Run(const Runtime::ServiceTable* services, const void* moduleState, Runtime::EventWriter* writer, int path) const override\
     {\
-        runCOre(services, moduleState, writer);\
+        runCOre(services, moduleState, writer, path);\
     }\
     const char* GetName() const override { return #name; }\
     static const Engine::Core::Scripting::ApiEventBase* GetEvent()\
@@ -20,9 +22,9 @@ public:\
 class name : Engine::Core::Scripting::ApiEvent_1<tp1>\
 {\
 protected:\
-    void RunCore(const Engine::Core::Runtime::ServiceTable* services, const void* moduleState, const tp1* p1, Engine::Core::Runtime::EventWriter* writer) const override \
+    void RunCore(const Engine::Core::Runtime::ServiceTable* services, const void* moduleState, const tp1* p1, Engine::Core::Runtime::EventWriter* writer, int path) const override \
     {\
-        runCore(services, moduleState, p1, writer);\
+        runCore(services, moduleState, p1, writer, path);\
     }\
 public:\
     const char* GetName() const override { return #name; }\
